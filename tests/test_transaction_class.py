bdd974a230e7cee71312fa4013f1318a4421278c
mzamolilitha19 <mzamolilitha19@gmail.com>
2026-02-25 13:46:39 +0200

    Tutorial 4 completed

--- tests/test_transaction_class.py
+++ tests/test_transaction_class.py
@@ -57,16 +57,25 @@ def test_empty_transactions():
     assert calculate_total_expenses(empty_list) == Decimal('0')
     assert calculate_total_income(empty_list) == Decimal('0')
     assert calculate_balance(empty_list) == Decimal('0')
-    assert check_financial_health(empty_list) == "Overspending"  # Since income is 0
+    assert check_financial_health(empty_list) == "No transactions recorded"  # Since income is 0
 
 #TODO Complete this test
 def test_only_expenses(expenses_only_transactions):
-    pass
-
+    assert calculate_total_income(expenses_only_transactions) == Decimal('0')
+    assert calculate_total_expenses(expenses_only_transactions) == Decimal('-1000.00')
+    assert calculate_balance(expenses_only_transactions) == Decimal('-1000.00')
+    assert check_financial_health(expenses_only_transactions) == "Overspending"
 #TODO Complete this test
 def test_only_income(income_only_transactions):
-    pass
+    assert calculate_total_income(income_only_transactions) == Decimal('3500.00')
+    assert calculate_total_expenses(income_only_transactions) == Decimal('0')
+    assert calculate_balance(income_only_transactions) == Decimal('3500.00')
+    assert check_financial_health(income_only_transactions) == "Saving well"
 
 #TODO Complete this test
 def test_mixed_transactions(sample_transactions):
-    pass
+    assert calculate_total_income(sample_transactions) == Decimal('11500.00')
+    assert calculate_total_expenses(sample_transactions) == Decimal('-4750.50')
+    assert calculate_balance(sample_transactions) == Decimal('6749.50')
+    assert check_financial_health(sample_transactions) == "Saving well"
+    
